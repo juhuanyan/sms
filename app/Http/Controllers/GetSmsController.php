80fe2d225939a2a8c554f8cc3df50404ae78d5e4
listen <hi_listen@qq.com>
2017-01-14 22:27:02 +0800

    no message

--- app/Http/Controllers/GetSmsController.php
+++ app/Http/Controllers/GetSmsController.php
@@ -13,7 +13,7 @@ class GetSmsController extends Controller
 {
     public function index(){
 
-        $jiekou = Jiekou::where(['name'=>'sms2'])->first();
+        $jiekou = Jiekou::where(['name'=>'sms3'])->first();
         $response = Curl::to($jiekou->url)
             ->get();
         dd($response);
