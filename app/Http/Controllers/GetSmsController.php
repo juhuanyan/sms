a33d2177428bed6facd40fdfd7f0207e87c638e2
listen <hi_listen@qq.com>
2017-02-16 10:27:48 +0800

    no message

--- app/Http/Controllers/GetSmsController.php
+++ app/Http/Controllers/GetSmsController.php
@@ -85,28 +85,29 @@ class GetSmsController extends Controller
     }
     public function customerSms(Request $request, $username) {
         $user = AdminUser::where(['username'=>$username])->first();
+        $jiekouid = explode(',', $user->jiekouid);
         $realip = $this->get_real_ip();
         $fangwenip = explode(',', $user->fangwenip);
         if (($fangwenip && in_array($realip, $fangwenip) || !$user->fangwenip)){
             $sdt = $request->sdt;
             $edt = $request->edt;
             if ($sdt && !$edt){
-                $smss = Smss::where(['jiekouid'=>$user->jiekouid])
+                $smss = Smss::whereIn('jiekouid', $jiekouid)
                     ->where('deliverdate', '>=', $sdt.' 00:00:00')
                     ->select('caller','msg','deliverdate')
                     ->get();
             } elseif(!$sdt && $edt){
-                $smss = Smss::where(['jiekouid'=>$user->jiekouid])
+                $smss = Smss::whereIn('jiekouid', $jiekouid)
                     ->where('deliverdate', '<=', $edt.' 23:59:59')
                     ->select('caller','msg','deliverdate')
                     ->get();
             } elseif($sdt && $edt) {
-                $smss = Smss::where(['jiekouid'=>$user->jiekouid])
+                $smss = Smss::whereIn('jiekouid', $jiekouid)
                     ->whereBetween('deliverdate', [$sdt.' 00:00:00', $edt.' 23:59:59'])
                     ->select('caller','msg','deliverdate')
                     ->get();
             } else {
-                $smss = Smss::where(['jiekouid'=>$user->jiekouid])->select('caller','msg','deliverdate')->get();
+                $smss = Smss::whereIn('jiekouid', $jiekouid)->select('caller','msg','deliverdate')->get();
             }
 
             return response($smss);
